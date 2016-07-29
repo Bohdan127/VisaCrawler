83a6f2ba25ad7c0b34ffa214f633b667b08e4531
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2016-07-29 16:41:11 +0200

    Move state manager into BusinessLogic from WinForms

--- Visa/Visa..BusinessLogic/Managers/StateManager.cs
+++ Visa/Visa..BusinessLogic/Managers/StateManager.cs
@@ -1,28 +1,37 @@
-﻿using System.Json;
+﻿using NLog;
+using System.Json;
 using System.Net;
 
-namespace Visa.WinForms.Managers
+namespace Visa.BusinessLogic.Managers
 {
     public class StateManager
     {
+        private static readonly Logger _logger =
+            LogManager.GetCurrentClassLogger();
+
         //todo maybe better to move this link into options???
         protected string ScriptUrl = "http://wiknaopen.esy.es/visa/out.json";
 
         public bool GetCurrentSiteAvailability()
         {
+            _logger.Trace("Start GetCurrentSiteAvailability.");
+
             var bRes = GetScriptResult(ScriptUrl);
 
+            _logger.Trace($"End GetCurrentSiteAvailability. bRes = {bRes}");
             return bRes;
         }
 
         protected virtual bool GetScriptResult(string scriptUrl)
         {
+            _logger.Trace($"Start GetScriptResult. scriptUrl = {scriptUrl}");
             JsonValue json;
             using (var wc = new WebClient())
             {
                 var jsonString = wc.DownloadString(ScriptUrl);
                 json = JsonValue.Parse(jsonString);
             }
+            _logger.Trace($"End GetScriptResult. json[\"SiteStatus\"] = {json["SiteStatus"]}");
             return json["SiteStatus"].ToString() == "1";
         }
     }
