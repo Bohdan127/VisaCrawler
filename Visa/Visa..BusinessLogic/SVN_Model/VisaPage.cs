1d66aa1b0d8c848142007977d37ad90b9aba76a4
AgentIvan <AgentIvan@gmail.com>
2016-08-20 00:09:32 +0300

    Add other Pages

--- Visa/Visa..BusinessLogic/SVN_Model/VisaPage.cs
+++ Visa/Visa..BusinessLogic/SVN_Model/VisaPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 //using System.Collections.Generic;
@@ -26,6 +26,12 @@ namespace Visa.BusinessLogic.SVN_Model
 // (S(ems55hr1n1behuikzzbvxbnr))/AppScheduling/AppSchedulingGetInfo.aspx?p=s2x6znRcBRv7WQQK7h4MTjZiPRbOsXKqJzddYBh3qCA%3d
         [Description("AppSchedulingReceiptDetails")]
         AppSchedulingReceiptDetails = 4,
+        [Description("AppEmailRegistration")]
+        AppEmailRegistration = 5,
+        [Description("AppSchedulingVisaCategory")]
+        AppSchedulingVisaCategory = 6,
+        [Description("AppSchedulingInterviewDate")]
+        AppSchedulingInterviewDate = 7,
         [Description("Sessionexpiry")]
         Sessionexpiry = 5
     }
@@ -38,12 +44,15 @@ namespace Visa.BusinessLogic.SVN_Model
         public static IDictionary<string, VisaPage> PageCodeDic
         = new Dictionary<string, VisaPage>()
         {
-            { "None.None", (VisaPage)1},
-            { "AppWelcome.AppWelcome", (VisaPage)2},
-            {  "AppScheduling.AppScheduling", (VisaPage)3},
-            { "AppSchedulingGetInfo.AppSchedulingGetInfo", (VisaPage)4},
-            { "AppSchedulingReceiptDetails.AppSchedulingReceiptDetails", (VisaPage)5},
-            { "Sessionexpiry", (VisaPage)5}
+            { "None.None", (VisaPage)0},
+            { "AppWelcome.AppWelcome", (VisaPage)1},
+            {  "AppScheduling.AppScheduling", (VisaPage)2},
+            { "AppSchedulingGetInfo.AppSchedulingGetInfo", (VisaPage)3},
+            { "AppSchedulingReceiptDetails.AppSchedulingReceiptDetails", (VisaPage)4},
+            { "AppEmailRegistration.AppEmailRegistration", (VisaPage)5},
+            { "AppSchedulingVisaCategory.AppSchedulingVisaCategory", (VisaPage)6},
+            { "AppSchedulingInterviewDate.AppSchedulingInterviewDate", (VisaPage)7},
+            { "Sessionexpiry", (VisaPage)8}
         };
         public static string[] pages = new string[]{
             "None.None",
@@ -51,6 +60,9 @@ namespace Visa.BusinessLogic.SVN_Model
             "AppScheduling.AppScheduling",
             "AppSchedulingGetInfo.AppSchedulingGetInfo",
             "AppSchedulingReceiptDetails.AppSchedulingReceiptDetails",
+            "AppEmailRegistration.AppEmailRegistration",
+            "AppSchedulingVisaCategory.AppSchedulingVisaCategory",
+            "AppSchedulingInterviewDate.AppSchedulingInterviewDate",
             "Sessionexpiry"
         };
     }
